d2c5b7eae6397caa9b804b73bc743eb879b46d21
chubinho <andrewchubinets07@yandex.ru>
2025-11-13 18:09:10 +0300

    improve  get_user, table users and UserOut

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,24 @@
 from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
 
 class UserRegister(BaseModel):
     email: EmailStr
     password: str = Field(min_length=8, max_length=72)
+    phone: Optional[str] = Field(None,pattern= r"^\+?[1-9]\d{1,14}$")
+    name: Optional[str] = None
+
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+
 class UserOut(BaseModel):
-    id: int
-    email: str
+    name: str
+    email: EmailStr
+    phone: Optional[str] = None
+    date: str
+
+    class Config:
+        from_attributes = True
