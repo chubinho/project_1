67f80ce9ef8c4d291525373215e49fdb973de4c0
chubinho <andrewchubinets07@yandex.ru>
2025-11-15 17:26:54 +0300

    update /ads(get)

--- app/routes/ad.py
+++ app/routes/ad.py
@@ -3,6 +3,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Optional, Annotated
 from datetime import datetime
+from math import radians, sin, asin, sqrt, cos
 
 from ..dependencies import get_current_user
 from ..database import get_session
@@ -12,6 +13,7 @@ from ..schemas import AdOut, AdCreate
 router = APIRouter()
 sessionDep = Annotated[AsyncSession, Depends(get_session)]
 userDep = Annotated[User, Depends(get_current_user)]
+
 
 @router.post("/ads")
 async def create_ad(data: AdCreate, session: sessionDep, current_user: userDep):
@@ -45,6 +47,20 @@ async def create_ad(data: AdCreate, session: sessionDep, current_user: userDep):
 
     return {"success": True, "ad_id": ad.id}
 
+
+def haversine(lat1, lon1, lat2, lon2):
+    """Вычисление расстояния между двумя точками"""
+    R = 6371
+    d_lat = radians(lat2 - lat1)
+    d_lon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    a = sin(d_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(d_lon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    return R * c
+
+
 @router.get("/ads")
 async def get_ads(
     status: Optional[str] = None,
@@ -52,6 +68,9 @@ async def get_ads(
     breed: Optional[str] = None,
     size: Optional[str] = None,
     danger: Optional[str] = None,
+    region: Optional[str] = None,
+    geoloc: Optional[str] = "any",
+    radius: Optional[int] = None,
     session: AsyncSession = Depends(get_session)
 ):
     try:
@@ -72,8 +91,34 @@ async def get_ads(
 
         result = await session.scalars(query)
         ads = result.all()
+        filtered_ads = []
+        if geoloc != "any":
+            try:
+                user_lat, user_lon = map(float, geoloc.split(","))
+            except:
+                return {"success": False, "message": "Неверный формат geoloc"}
 
-        ads_out = [AdOut.model_validate(ad) for ad in ads]
+            for ad in ads:
+                if not ad.geoLocation:
+                    continue
+
+                try:
+                    ad_lat, ad_lon = map(float, ad.geoLocation.split(","))
+                except:
+                    continue
+
+                distance = haversine(user_lat, user_lon, ad_lat, ad_lon)
+
+                if radius is None or distance <= radius:
+                    filtered_ads.append(ad)
+        else:
+            if region:
+                for ad in ads:
+                    if ad.location and region.lower() in ad.location.lower():
+                        filtered_ads.append(ad)
+            else:
+                filtered_ads = []
+        ads_out = [AdOut.model_validate(ad) for ad in filtered_ads]
 
         return {"success": True, "ads": ads_out}
 
